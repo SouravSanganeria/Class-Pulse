e567efc1bb351a837a5b13d72be18871c05bbc78
veeresh09 <veeresh1729@gmail.com>
2020-06-18 12:26:18 +0530

    Added socket.io

--- src/components/StudentSlides.jsx
+++ src/components/StudentSlides.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Button, ButtonToolbar, Container } from "react-bootstrap";
 import { Alert } from "reactstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
+import socket from "../socket";
 //import { Icon } from "@stardust-ui/react";
 //import { axiosGET, axiosPOST } from "../utils/axiosClient";
 import {
@@ -15,14 +16,14 @@ import {
   Input,
   Modal,
   ModalHeader,
-  ModalBody
+  ModalBody,
 } from "reactstrap";
 import axios from "axios";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${
   pdfjs.version
 }/pdf.worker.js`;
 
-class StudentSlides extends Component {
+class StudentSlide extends Component {
   constructor(props) {
     super(props);
     this.markoff = this.markoff.bind(this);
@@ -34,11 +35,25 @@ class StudentSlides extends Component {
     this.getmarks = this.getmarks.bind(this);
     this.decPage = this.decPage.bind(this);
     this.incPage = this.incPage.bind(this);
+    this.tcpageno = this.props.x;
     this.onDocumentLoadSuccess = this.onDocumentLoadSuccess.bind(this);
   }
   componentDidMount() {
     console.log("Slides Reached");
+    console.log(this.state.tcpageno);
+    console.log(this.props.x);
     this.getmarks();
+  }
+  componentDidUpdate(prevProps) {
+    console.log("Slides Reached");
+    console.log(this.props.x);
+    console.log(this.state.tcpageno);
+    if (prevProps.x !== this.props.x) {
+      this.setState({ tcpageno: this.props.x });
+      if (this.props.x >= 1 && this.props.x < this.state.numPages) {
+        this.setState({ pageNumber: this.props.x }, this.getmarks);
+      }
+    }
   }
   state = {
     file: localStorage.getItem("link"),
@@ -50,24 +65,23 @@ class StudentSlides extends Component {
     form_completed: false,
     modal: false,
     Xcord: 0,
-    Ycord: 0
+    Ycord: 0,
   };
 
   toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
+    this.setState((prevState) => ({
+      modal: !prevState.modal,
     }));
   }
 
   onChangeFormColour(e) {
     this.setState({
-      form_colour: e.target.value
+      form_colour: e.target.value,
     });
   }
-
   onChangeComment(e) {
     this.setState({
-      form_comment: e.target.value
+      form_comment: e.target.value,
     });
   }
 
@@ -78,7 +92,7 @@ class StudentSlides extends Component {
         Xcord: this.state.Xcord,
         Ycord: this.state.Ycord,
         colour: this.state.form_colour,
-        comment: this.state.form_comment
+        comment: this.state.form_comment,
       };
       //console.log("onsubmit reached");
       //console.log("ssid", this.state.sid);
@@ -88,7 +102,7 @@ class StudentSlides extends Component {
           `api/marks/add/${this.state.sid}/${this.state.pageNumber}`,
           newMark
         )
-        .then(res => console.log("okay", res.data));
+        .then((res) => console.log("okay", res.data));
 
       this.setState({ form_completed: false });
     }
@@ -101,7 +115,7 @@ class StudentSlides extends Component {
     this.setState({
       Xcord: e.nativeEvent.offsetX,
       Ycord: e.nativeEvent.offsetY,
-      form_completed: true
+      form_completed: true,
     });
     this.toggle();
   }
@@ -110,7 +124,7 @@ class StudentSlides extends Component {
     var url = `api/marks/getSlide/${this.state.sid}/${this.state.pageNumber}`;
     axios
       .get(url)
-      .then(response => {
+      .then((response) => {
         console.log("response", response.data.length);
         const newX = [];
         const newY = [];
@@ -128,7 +142,7 @@ class StudentSlides extends Component {
           markx: newX,
           marky: newY,
           colour: newColour,
-          comment: newComment
+          comment: newComment,
         });
       })
       .catch(function(error) {
@@ -169,11 +183,16 @@ class StudentSlides extends Component {
             top: this.state.marky[i],
             left: this.state.markx[i],
             position: "absolute",
-            zIndex: 2
+            zIndex: 2,
           }}
         />
       );
     }
+    // console.log("something happened");
+    // socket.on("turnPage", (msg) => {
+    //   console.log(msg);
+    //   console.log("something happened");
+    // });
     return (
       <Container>
         <Row>
@@ -185,13 +204,13 @@ class StudentSlides extends Component {
           style={{
             background: "AliceBlue",
             position: "relative",
-            height: "900px"
+            height: "900px",
           }}
         >
           <Row
             style={{
               padding: "15px",
-              height: "850px"
+              height: "850px",
             }}
           >
             <Col>
@@ -203,7 +222,7 @@ class StudentSlides extends Component {
                   height: "500px",
                   width: "500px",
                   top: "20px",
-                  left: "0%"
+                  left: "0%",
                 }}
               >
                 <Document
@@ -231,7 +250,7 @@ class StudentSlides extends Component {
                     style={{
                       display: "inline",
                       position: "relative",
-                      left: "40%"
+                      left: "40%",
                     }}
                   >
                     Page {pageNumber} of {numPages}
@@ -303,6 +322,18 @@ class StudentSlides extends Component {
       </Container>
     );
   }
+}
+
+function StudentSlides() {
+  const [x, setX] = useState(1);
+  useEffect(() => {
+    socket.on("turnPage", (msg) => {
+      console.log(msg);
+      setX(msg);
+      console.log("something happened");
+    });
+  });
+  return <StudentSlide x={x} />;
 }
 
 export default StudentSlides;
